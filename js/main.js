bfe8d6278ad818c56dfe9ebac9e3f9dd983c36b2
davidetaribelli <davide.taribelli@gmail.com>
2023-04-30 15:09:12 +0200

    add js for check number

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 
 // infine il software deve dire quanti numeri sono stati indovinati
 // prendere i numeri scritti dall'utente e confrontarli con i numeri generati in precedenza
+// una volta confrontato i due fare la differenza dei numeri azzeccati con quelli generati per trovare quanti ne sono stati azzeccati e quali
 
 //FUNZIONI
 
@@ -8,6 +9,7 @@
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * ((max + 1) - min) + min);   
 }
+
 // funzione per generare dei numeri da stampare
 function genNum (quantita){
     let numbers = [];
@@ -25,25 +27,38 @@ function genNum (quantita){
 //funzione che nasconde i numeri
 function hideNum(){
     numeriEl.innerHTML = "";
+    setTimeout(inputUtente, 1 * 1000);
 }
 
 // funzione che genera i prompt
 function inputUtente(){
+    let utente = []; 
     for(let i = 0; i < 5; i++){
         let numUtente = parseInt(prompt("Inserisci i numeri che ricordi uno alla volta"));
         if (isNaN(numUtente)){
             alert("devi inserire un numero");
         }
-    
+        utente.push(numUtente);
+
+        let correctNumber = [];
+        for (let i = 0; i < 5; i++) {
+            if (num.includes(utente[i])) {
+                correctNumber.push(utente[i]);
+            }
+        }
+        alert("Hai indovinato " + correctNumber.length + " numeri: " + correctNumber.join(", "));
     }
 }
+
+
+
 
 // VARIABILI
 
 // dichiaro le mie variabili
 const numeriEl = document.querySelector(".numeri");
-let num = genNum("5")
-
+let num = genNum(5);
+console.log("Numeri generati: ", num);
 // DOM
 
 //creo un paragrafo dove stampo i numeri e lo aggiungo al suo contenitore
@@ -51,9 +66,15 @@ let printNum = document.createElement("p");
 printNum.innerHTML = num;
 numeriEl.appendChild(printNum);
 
-// dopo 30 secondi i numeri scompaiono grazie alla funzione dichiarata in precedenza
-setTimeout(hideNum, 30 * 1000);
-// dopo 32 secondi appaiono i prompt
-setTimeout(inputUtente, 31 * 1000);
+// dopo 3 secondi i numeri scompaiono grazie alla funzione dichiarata in precedenza
+setTimeout(hideNum, 3 * 1000);
+
+
+
+
+
+
+
+
 
 
